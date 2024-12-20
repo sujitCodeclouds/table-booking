af0ce08238857859eb57daf3eea0cb21a48ead56
sujitCodeclouds <sujit.mondal@codeclouds.co.in>
2024-12-20 16:55:50 +0530

    update

--- src/pages/RestaurantDetails.jsx
+++ src/pages/RestaurantDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useParams, Link, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { restaurants } from "../data/restaurants";
 
 const RestaurantDetails = () => {
